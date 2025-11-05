df674346a99fa2df72a954c03d2023631278405f
BTavkhelidze <beka.tavkhelidze@leverx.com>
2025-11-05 20:46:41 +0400

    feat/ dynamicPage Content

--- webapp/controller/ListReport.controller.js
+++ webapp/controller/ListReport.controller.js
@@ -1,10 +1,18 @@
-sap.ui.define([
-    "sap/ui/core/mvc/Controller"
-], (Controller) => {
-    "use strict";
+sap.ui.define(['sap/ui/core/mvc/Controller'], (Controller) => {
+  'use strict';
 
-    return Controller.extend("freestylesapui5app.controller.ListReport", {
-        onInit() {
-        }
-    });
+  return Controller.extend('freestylesapui5app.controller.ListReport', {
+    onInit() {
+      const oODataModel = this.getView().getModel();
+      const oModelFromComp = this.getOwnerComponent().getModel();
+
+      oModelFromComp.read('/Stores', {
+        success: (oData) => {
+          const oJsonModel = new sap.ui.model.json.JSONModel(oData.results);
+          console.log(oJsonModel);
+          this.getView().setModel(oJsonModel, 'Stores');
+        },
+      });
+    },
+  });
 });
