fd813e0cd0cb1e076c0f9590bde5473cc7b9d15b
BTavkhelidze <beka.tavkhelidze@leverx.com>
2025-11-06 18:03:42 +0400

    feat/ filter, i18n. improve/ Table

--- webapp/controller/ListReport.controller.js
+++ webapp/controller/ListReport.controller.js
@@ -1,18 +1,43 @@
-sap.ui.define(['sap/ui/core/mvc/Controller'], (Controller) => {
-  'use strict';
+sap.ui.define(
+  [
+    'sap/ui/core/mvc/Controller',
+    'sap/ui/model/Filter',
+    'sap/ui/model/FilterOperator',
+    'sap/ui/model/json/JSONModel',
+  ],
+  (Controller, Filter, FilterOperator, JSONModel) => {
+    'use strict';
 
-  return Controller.extend('freestylesapui5app.controller.ListReport', {
-    onInit() {
-      const oODataModel = this.getView().getModel();
-      const oModelFromComp = this.getOwnerComponent().getModel();
+    return Controller.extend('freestylesapui5app.controller.ListReport', {
+      onInit() {
+        const oModelFromComp = this.getOwnerComponent().getModel();
+        oModelFromComp.read('/Stores', {
+          success: (oData) => {
+            const oJsonModel = new JSONModel(oData.results);
+            this.getView().setModel(oJsonModel, 'Stores');
+          },
+        });
+      },
 
-      oModelFromComp.read('/Stores', {
-        success: (oData) => {
-          const oJsonModel = new sap.ui.model.json.JSONModel(oData.results);
-          console.log(oJsonModel);
-          this.getView().setModel(oJsonModel, 'Stores');
-        },
-      });
-    },
-  });
-});
+      onGoButtonPressed() {
+        const oFilter = this.getView().byId('filterbar');
+
+        const aFilterItems = oFilter.getFilterGroupItems();
+        const aFilters = [];
+
+        aFilterItems.forEach((oItem) => {
+          const sName = oItem.getName();
+          const oControl = oItem.getControl();
+          const sValue = oControl.getValue && oControl.getValue();
+          if (sValue) {
+            aFilters.push(new Filter(sName, FilterOperator.Contains, sValue));
+          }
+        });
+
+        const oList = this.byId('StoresTable');
+        const oBinding = oList.getBinding('rows');
+        oBinding.filter(aFilters);
+      },
+    });
+  }
+);
