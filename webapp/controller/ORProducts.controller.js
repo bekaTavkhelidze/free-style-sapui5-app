43d7c3c7b2f9984f57b4df8f7566406807fb841f
BTavkhelidze <beka.tavkhelidze@leverx.com>
2025-11-10 15:24:53 +0400

    improve/ OR page Structure: table, header

--- webapp/controller/ORProducts.controller.js
+++ webapp/controller/ORProducts.controller.js
@@ -1,21 +1,40 @@
-sap.ui.define(['sap/ui/core/mvc/Controller'], (Controller) => {
-  'use strict';
+sap.ui.define(
+  [
+    'sap/ui/core/mvc/Controller',
+    'sap/ui/model/Filter',
+    'sap/ui/model/FilterOperator',
+  ],
+  (Controller, Filter, FilterOperator) => {
+    'use strict';
 
-  return Controller.extend('freestylesapui5app.controller.ORProducts', {
-    onInit() {
-      const oRouter = this.getOwnerComponent().getRouter();
-      console.log(oRouter);
-      console.log(oRouter.getRoute('ORProducts'));
-      oRouter
-        .getRoute('ORProducts')
-        .attachPatternMatched(this.onObjectMatched, this);
-    },
-    onObjectMatched(oEvent) {
-      var activeId = window.decodeURIComponent(
-        oEvent.getParameter('arguments').id
-      );
+    return Controller.extend('freestylesapui5app.controller.ORProducts', {
+      onInit() {
+        const oRouter = this.getOwnerComponent().getRouter();
 
-      console.log(activeId);
-    },
-  });
-});
+        oRouter
+          .getRoute('ORProducts')
+          .attachPatternMatched(this.onObjectMatched, this);
+      },
+      onObjectMatched(oEvent) {
+        const activeId = globalThis.decodeURIComponent(
+          oEvent.getParameter('arguments').id
+        );
+
+        const ODataModel = this.getOwnerComponent().getModel();
+
+        ODataModel.read("/Stores(guid'" + activeId + "' )", {
+          urlParameters: {
+            $expand: 'Products',
+          },
+          success: (oData) => {
+            console.log(oData);
+            const oContext = ODataModel.createBindingContext(
+              "/Stores(guid'" + activeId + "')"
+            );
+            this.getView().setBindingContext(oContext);
+          },
+        });
+      },
+    });
+  }
+);
